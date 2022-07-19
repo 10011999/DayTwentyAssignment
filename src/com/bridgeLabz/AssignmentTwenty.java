d80ddb22f3aa1f7d07f0a803e18a1a7e9c38e606
10011999 <motewaraditya77@gmail.com>
2022-07-19 16:26:31 +0530

    Modified:Last Name

--- src/com/bridgeLabz/AssignmentTwenty.java
+++ src/com/bridgeLabz/AssignmentTwenty.java
@@ -7,6 +7,7 @@ public class AssignmentTwenty {
     public static void main(String[] args) {
         AssignmentTwenty pattern = new AssignmentTwenty();
         pattern.firstName();
+        pattern.lastName();
 
     }
     public static void firstName() {
@@ -14,6 +15,16 @@ public class AssignmentTwenty {
         Pattern pattern1 = Pattern.compile("^[A-Z][a-z]{3,}$");
         Matcher matcher1 = pattern1.matcher("Aditya");
         if (matcher1.find())
+            System.out.println("valid Name");
+        else
+            System.out.println("invalid Name");
+    }
+
+    public static void lastName() {
+        System.out.println("=== Last Name ====");
+        Pattern pattern2 = Pattern.compile("^[A-Z][a-z]{3,}$");
+        Matcher matcher2 = pattern2.matcher("Motewar");
+        if (matcher2.find())
             System.out.println("valid Name");
         else
             System.out.println("invalid Name");
